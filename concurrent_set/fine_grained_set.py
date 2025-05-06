05fbbb0fa5222bc96b269122521390327cf3f2e2
KhanhMHQ <qkhanhpro99@gmail.com>
2025-05-07 01:02:09 +0700

    update: parallel astar function

--- concurrent_set/fine_grained_set.py
+++ concurrent_set/fine_grained_set.py
@@ -8,54 +8,67 @@ class Node:
 
 class FineGrainedSet:
     def __init__(self):
-        self.head = Node(float('-inf'))
-        self.tail = Node(float('inf'))
+        self.head = Node(None)
+        self.tail = Node(None)
         self.head.next = self.tail
 
     def add(self, value):
-        pred, curr = self.head, self.head.next
+        pred = self.head
         pred.lock.acquire()
+        curr = pred.next
         curr.lock.acquire()
         try:
-            while curr.value < value:
+            while curr.value is not None and value < curr.value:
                 pred.lock.release()
                 pred, curr = curr, curr.next
                 curr.lock.acquire()
+
             if curr.value == value:
                 return False
-            node = Node(value)
-            node.next = curr
-            pred.next = node
+
+            new_node = Node(value)
+            new_node.next = curr
+            pred.next = new_node
             return True
         finally:
-            pred.lock.release()
             curr.lock.release()
+            pred.lock.release()
 
     def remove(self, value):
-        pred, curr = self.head, self.head.next
+        pred = self.head
         pred.lock.acquire()
+        curr = pred.next
         curr.lock.acquire()
         try:
-            while curr.value < value:
+            while curr.value is not None and value < curr.value:
                 pred.lock.release()
                 pred, curr = curr, curr.next
                 curr.lock.acquire()
+
             if curr.value != value:
                 return False
+
             pred.next = curr.next
             return True
         finally:
-            pred.lock.release()
             curr.lock.release()
+            pred.lock.release()
 
     def contains(self, value):
         curr = self.head
         curr.lock.acquire()
-        while curr.value < value:
-            pred = curr
-            curr = curr.next
-            curr.lock.acquire()
-            pred.lock.release()
-        result = curr.value == value
-        curr.lock.release()
-        return result
+        try:
+            next_node = curr.next
+            next_node.lock.acquire()
+            try:
+                while next_node.value is not None and value < next_node.value:
+                    curr.lock.release()
+                    curr = next_node
+                    next_node = curr.next
+                    next_node.lock.acquire()
+
+                return next_node.value == value
+            finally:
+                next_node.lock.release()
+        finally:
+            curr.lock.release()
